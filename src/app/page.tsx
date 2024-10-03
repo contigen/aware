c2422ec4076e499fc93578814fb6ea39c47c7582
contigen <69436315+contigen@users.noreply.github.com>
2024-10-03 23:22:49 +0100

    Remove needless components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { ScamDetection } from '&/components/scam-detection'
+import { Button } from '&/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <ScamDetection />
+    <div className=''>
+      <main className='flex-grow container mx-auto p-8'>
+        <div className='text-center'>
+          <h2 className='text-4xl font-bold mb-6'>Stay Safe from Scams</h2>
+          <p className='text-xl mb-8'>
+            Aware AI-powered tool helps you identify potential scams in emails
+            and messages.
+          </p>
+          <Button size='lg' asChild>
+            <Link href='/detect'>Get Started</Link>
+          </Button>
+        </div>
+      </main>
     </div>
   )
 }
