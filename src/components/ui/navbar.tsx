7d3c635b142ab9912fea1db01f15ef25746c9e3a
contigen <69436315+contigen@users.noreply.github.com>
2024-10-03 23:28:42 +0100

    Add link to home page logo

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -30,10 +30,12 @@ export function Navbar() {
   return (
     <header className='bg-blue-600 text-white p-4'>
       <nav className='container mx-auto flex justify-between items-center'>
-        <div className='flex items-center gap-2'>
-          <Shield className='w-8 h-8' />
-          <h2 className='text-2xl font-bold'>Aware</h2>
-        </div>
+        <Link href='/'>
+          <span className='inline-flex items-center gap-2'>
+            <Shield className='w-8 h-8' />
+            <h2 className='text-2xl font-bold'>Aware</h2>
+          </span>
+        </Link>
         <ul className='flex space-x-4'>
           {ROUTES.map(route => (
             <li key={route.name}>
